ccf3f9a168ac43227668b5e6ee4fc8f456deb1d9
ctrlshiftz <mahdi_mohammadi@outlook.com>
2022-01-16 22:17:43 +0330

    Part 26

--- src/Components/ImageList.jsx
+++ src/Components/ImageList.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const ImageList = ({ images }) => {
-	return <div>image list</div>;
+	const photos = images.map((image) => {
+		return <img key={image.id} src={image.urls.regular} alt={image.alt_description} />;
+	});
+	console.log(images);
+	return <div>{photos}</div>;
 };
 
 export default ImageList;
